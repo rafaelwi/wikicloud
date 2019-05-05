6adb48ac362302dd97e167589fe0d362b9d83e4b
rafaelwi <rafwiskailnicki@gmail.com>
2019-05-05 19:51:14 -0400

    Remove unnecessary libs

--- cloud.py
+++ cloud.py
@@ -2,18 +2,8 @@
 # github: rafaelwi
 
 """ Libraries """
-# For word cloud
-
-#import mathplotlib.pyplit as plt
-
 # For reading the page
 import wcfuncts as wc 
-
-# System Functions
-from os import path
-import urllib.request
-import sys
-
 
 """ Main Program """
 url = 'https://en.wikipedia.org/wiki/Death_Grips'
